363adf7fc46e550bade960397c9c8315f57670d6
wujunjie <wujunjie@youzan.com>
2021-04-25 10:09:14 +0800

    feat: demo

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,16 @@
       且随疾风前行，身后亦需流心
     </div>
 
-    <button v-print="'#printDiv1'">只打印指定 DOM（指令）</button>
+    <button v-print="'#printDiv1'">只打印指定 DOM（指令v-print="'#printDiv1'"）</button>
 
     <div id="printDiv2">
       谎言并不伤人，真相才是快刀
     </div>
 
-    <button v-print="printObj">只打印指定 DOM（对象）</button>
+    <button v-print="printObj">只打印指定 DOM（对象printObj）</button>
+
+    <h1>Demo源码</h1>
+    <a href="https://github.com/alexwjj/vue-iframe-print/blob/master/src/App.vue" target="_blank" rel="noopener noreferrer">https://github.com/alexwjj/vue-iframe-print/blob/master/src/App.vue</a>
   </div>
 </template>
 <script>
@@ -22,6 +25,7 @@ export default {
   name: "app",
   data() {
     return {
+      diyHtml: '<h1 style="color: red">自定义测试局部打印</h1>',
       printObj: {
         id: "printDiv2",
         popTitle: "vue-iframe-print",
